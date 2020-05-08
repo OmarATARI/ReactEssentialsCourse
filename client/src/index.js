3585cca4c41015a1b414010f0403b5e8a739e68d
Omar ATARI <oatari@bluegreen.com>
2020-05-08 23:18:36 +0200

    Add Bonjour component

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 /*ReactDOM.render(<App />, document.getElementById('root'));*/
-
+class Bonjour extends React.Component{
+        render() {
+            return (
+                <div>
+                    <h1>Bonjour de Réact</h1>
+                </div>
+            )
+        }
+}
 //babel-standlone made for non-node projects, jsx is already integrated here
-ReactDOM.render(<h1> Bonjour de Réact </h1>,  document.getElementById('root'));
+ReactDOM.render(<Bonjour/>,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
