d132bbfe7fbe698e063bcdcffd5c24e65ac46d70
Omar ATARI <oatari@bluegreen.com>
2020-05-08 23:10:57 +0200

    Understanding jsx

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 /*ReactDOM.render(<App />, document.getElementById('root'));*/
-ReactDOM.render( React.createElement('h1', null, 'Bonjour de Réact'),  document.getElementById('root'))
+
+//babel-standlone made for non-node projects, jsx is already integrated here
+ReactDOM.render(<h1> Bonjour de Réact </h1>,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
