886060fa05dfd665ed631adb24fed829dfb427fd
Omar ATARI <oatari@bluegreen.com>
2020-05-08 23:32:27 +0200

    Add functionnal component

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 /*ReactDOM.render(<App />, document.getElementById('root'));*/
-class Bonjour extends React.Component{
-        render() {
-            return (
-                <div>
-                    <h1>Bonjour de Réact</h1>
-                </div>
-            )
-        }
-}
+const Bonjour = () => (
+    <div>
+        <h1>Bonjour de Réact</h1>
+    </div>
+)
+
 //babel-standlone made for non-node projects, jsx is already integrated here
 ReactDOM.render(<Bonjour/>,  document.getElementById('root'));
 
