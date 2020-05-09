e0333f112e5c860b5d9e4b7ed67bf1a693122105
Omar ATARI <atari.omar@gmail.com>
2020-05-09 03:10:32 +0200

    Work with properties

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import ToDoList from "./ToDoList";
 import NavBar from "./NavBar";
 import AddTask from "./AddTask";
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import initialData from '../initialData'
 
 
 class App extends React.Component{
@@ -13,7 +14,7 @@ class App extends React.Component{
                 <BrowserRouter>
                     <Switch>
                         <Route path="/add-task" component={AddTask}/>
-                        <Route path="/:filter?" component={ToDoList}/>
+                        <Route path="/:filter?" render={(props) => <ToDoList {...props} tasks={initialData} />} />
                     </Switch>
                     <NavBar />
                 </BrowserRouter>
