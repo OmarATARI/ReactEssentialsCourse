dee5e9859ecfdfc106e4f064ff95a8b506f1f43a
Omar ATARI <atari.omar@gmail.com>
2020-05-09 02:24:52 +0200

    Use react-router for AddTask

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToDoList from "./ToDoList";
 import NavBar from "./NavBar";
+import AddTask from "./AddTask";
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 
 class App extends React.Component{
     render() {
         return(
             <section id="todo">
-                <ToDoList />
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/add-task" component={AddTask}/>
+                        <Route path="/" component={ToDoList}/>
+                    </Switch>
+                </BrowserRouter>
                 <NavBar />
             </section>
         )
